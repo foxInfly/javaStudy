137ae586b4f49e2c2fc54cb9b4165a84d27514a7
puli <123@123.com>
2020-01-11 17:33:20 +0800

    添加kafkaconsumer

---
+++ kafka/src/main/java/com/pupu/consumer/MyConsumer.java
@@ -0,0 +1,43 @@
+package com.pupu.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author puli2
+ * @since 2020/1/3 17:12
+ */
+public class MyConsumer {
+
+    public static void main(String[] args) {
+        //1.创建Kafka producer的配置信息
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"zookeeper01:9092");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,true);
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"1000");//自动提交offset延迟,默认开启，1000ms
+        //key\value反序列化配置
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserializer");
+        //消费者组
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,"wst_rest");
+
+        //创建消费者
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        //订阅主题
+        consumer.subscribe(Arrays.asList("wst_msg_push"));
+        //拉取数据
+        while (true){
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(100);
+            //解析并打印ConsumerRecords
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                System.out.println(consumerRecord.key()+"--"+consumerRecord.value());
+            }
+
+        }
+    }
+}
