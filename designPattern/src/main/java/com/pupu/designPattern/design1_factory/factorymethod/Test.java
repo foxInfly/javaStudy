09a72c75f2d790d06791db6254aaaf9d564031bb
zn_lp <lipu@zgzhongnan.com>
2021-01-18 15:50:30 +0800

    第四次学习工厂方法模式

--- designPattern/src/main/java/com/pupu/designPattern/design1_factory/factorymethod/Test.java
+++ designPattern/src/main/java/com/pupu/designPattern/design1_factory/factorymethod/Test.java
@@ -1,14 +1,16 @@
 package com.pupu.designPattern.design1_factory.factorymethod;
 
-/**
+/**工厂方法:
+ *     一个类通过其子类来指定创建哪个对象；
+ *   缺点：累的个数容易过多，增加复杂度；增加系统的抽象性和理解难度；
  * @author : lipu
  * @since : 2020-09-01 22:33
  */
 public class Test {
     public static void main(String[] args) {
-        ICourseFactory factory = new PythonCourseFactory();
 
-        ICourse course = factory.create();
-        System.out.println(course.getName());
+        System.out.println(new PythonCourseFactory().create().getName());
+
+        System.out.println(new JavaCourseFactory().create().getName());
     }
 }
