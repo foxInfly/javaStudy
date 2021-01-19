9e5327e975e5eb30ea3cb76e356dbba10ddf23f3
zn_lp <lipu@zgzhongnan.com>
2021-01-19 14:51:24 +0800

    第四次学习delegate

--- designPattern/src/main/java/com/pupu/designPattern/design17_bridge/general/Test.java
+++ designPattern/src/main/java/com/pupu/designPattern/design17_bridge/general/Test.java
@@ -5,10 +5,12 @@ package com.pupu.designPattern.design17_bridge.general;
  */
 public class Test {
     public static void main(String[] args) {
-        // 来一个实现化角色
+        //1.来一个实现化角色
         IImplementor imp = new ConcreteImplementorA();
+
         // 来一个抽象化角色，聚合实现
         Abstraction abs = new RefinedAbstraction(imp);
+
         // 执行操作
         abs.operation();
     }
--- designPattern/src/main/java/com/pupu/designPattern/design9_delegate/general/Delegate.java
+++ designPattern/src/main/java/com/pupu/designPattern/design9_delegate/general/Delegate.java
@@ -1,18 +1,19 @@
 package com.pupu.designPattern.design9_delegate.general;
 
 import java.util.Random;
+
 /**
  * Delegater
  */
-public class Delegate implements Task{
+public class Delegate implements Task {
     public void doTask() {
         System.out.println("代理执行开始....");
 
-        Task task = null;
-        if (new Random().nextBoolean()){
-        task = new ConcreteA();
-        task.doTask();
-        }else{
+        Task task;
+        if (new Random().nextBoolean()) {
+            task = new ConcreteA();
+            task.doTask();
+        } else {
             task = new ConcreteB();
             task.doTask();
         }
