965fef3094508ce72ed6a2c6d931ce93f284673b
zn_lp <lipu@zgzhongnan.com>
2021-01-20 10:48:38 +0800

    反转单向单链表

---
+++ dataStrAndAlg/src/main/java/com/gp/linearList/linkedList/reverseList/ListNode.java
@@ -0,0 +1,19 @@
+package com.gp.linearList.linkedList.reverseList;
+
+import lombok.Data;
+
+/**单向单链表
+ * @author lp
+ * @since 2021-01-19 23:22:04
+ */
+@Data
+public class ListNode {
+    //属性
+    int val;
+    //指向下一个节点的索引（指针）
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ dataStrAndAlg/src/main/java/com/gp/linearList/linkedList/reverseList/ReverseLinkedList.java
@@ -0,0 +1,54 @@
+package com.gp.linearList.linkedList.reverseList;
+
+
+/**
+ * Reverse Linked List   单向单链表反转
+ *
+ * @author lp
+ * @since 2021-01-19 23:12:47
+ */
+public class ReverseLinkedList {
+
+
+
+    //ListNode: 1 2 3 4 5
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        int count = 0;
+        System.out.println("原始链表："+listListNode(head));
+        ListNode pre = head;//1->2
+        ListNode current = head.next;//2->3
+        pre.next = null;
+        System.out.println("第"+count+"轮:" +listListNode(pre));
+        while (current != null){
+            count++;
+            ListNode next = current.next;//3->4
+            current.next = pre;
+            pre = current;
+            current = next;
+            System.out.println("第"+count+"轮:" +listListNode(pre));
+        }
+        return pre;
+    }
+
+    public static String listListNode(ListNode head){
+        StringBuilder sb = new StringBuilder();
+        while (head.next != null){
+            sb.append(head.val).append("->").append(head.next.val).append(" ");
+            head = head.next;
+        }
+        sb.append(head.val).append("->nul");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode4 = new ListNode(4,listNode5);
+        ListNode listNode3 = new ListNode(3,listNode4);
+        ListNode listNode2 = new ListNode(2,listNode3);
+        ListNode listNode1 = new ListNode(1,listNode2);
+        reverseList(listNode1);
+    }
+}
