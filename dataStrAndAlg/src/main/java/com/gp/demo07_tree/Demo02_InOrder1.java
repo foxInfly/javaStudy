49b9941f9b41e64067fe44bedead176521356902
zn_lp <lipu@zgzhongnan.com>
2021-01-25 11:05:52 +0800

    in-order traversal

---
+++ dataStrAndAlg/src/main/java/com/gp/demo07_tree/Demo02_InOrder1.java
@@ -0,0 +1,47 @@
+package com.gp.demo07_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lp
+ * @since 2021-01-25 10:24:36
+ */
+public class Demo02_InOrder1 {
+
+    /**
+     * @param root: A Tree
+     *
+     * @return: in-order traversal in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        //这里设计的秒，刚好跳出递归
+        if(root == null) return result;
+
+        List<Integer> left = inorderTraversal(root.left);
+        List<Integer> right = inorderTraversal(root.right);
+
+        result.addAll(left);
+        result.add(root.val);
+
+        //通过是否修改原有集合来返回true或者false
+        boolean b = result.addAll(right);
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode7 = new TreeNode(7);
+        TreeNode treeNode9 = new TreeNode(9);
+        TreeNode treeNode12 = new TreeNode(12);
+        TreeNode treeNode5 = new TreeNode(5, treeNode1, treeNode7);
+        TreeNode treeNode10 = new TreeNode(10, treeNode9, treeNode12);
+        TreeNode treeNode8 = new TreeNode(8, treeNode5, treeNode10);
+
+
+        System.out.println(new Demo02_InOrder2().inorderTraversal(treeNode8));
+    }
+}
---
+++ dataStrAndAlg/src/main/java/com/gp/demo07_tree/Demo02_InOrder2.java
@@ -0,0 +1,31 @@
+package com.gp.demo07_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author lp
+ * @since 2021-01-25 10:24:54
+ */
+public class Demo02_InOrder2 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}
